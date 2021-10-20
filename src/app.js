16d4bbd235a338edb9f76da4425fe673d41e2a22
M.Akif <M.Akif@DESKTOP-HHK6G12>
2021-10-20 16:57:12 +0300

    18 ekim ders sonu

---
+++ src/app.js
@@ -0,0 +1,25 @@
+console.log("Merhaba Kodlama.io")
+
+let dolarDun=9.20
+let dolarBugun=9.30
+
+{
+    let dolarDun=9.10
+}
+
+console.log(dolarDun)
+
+const euroDun=8.25
+
+//euroDun=8.5
+
+console.log(euroDun)
+
+let konutKredileri=["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Araba Kredisi"]
+
+for (let i=0;i<konutKredileri.length;i++) {
+    
+    console.log(konutKredileri[i])
+}
+
+//console.log(konutKredileri)
